1d97acfab5a30601e5ef8f9724191e1f9493f9d5
LiChiHuang <109316113@gms.tcu.edu.tw>
2024-04-24 17:13:11 +0800

    240422

--- src/components/PrintContent.vue
+++ src/components/PrintContent.vue
@@ -1,102 +1,99 @@
 <template>
-  <div v-for="(data, index) in selectedDataArray" :key="index">
-    <div class="print-content">
-      <div class="section1">
-        <div class="s1-1">
-          <div class="some-content">
-            <p><b>【 限時專送 】</b></p>
-          </div>
-          <div class="s1-address">
-            <b
-              ><label>{{ selectedDataArray.address }}</label></b
-            >
-            <b v-if="selectedDataArray && selectedDataArray.parent">
-              {{ selectedDataArray.parent }}&nbsp;先生&nbsp;/&nbsp;女士&nbsp;收
-            </b>
-          </div>
-        </div>
-        <div class="s1-std">
-          <label>
-            <span>{{ departmentName[index] }}</span
-            >&nbsp;-&nbsp; <span>{{ selectedDataArray.degree }}</span
-            >&nbsp;-&nbsp; <span>{{ selectedDataArray.studentClass }}</span
-            >&nbsp;-&nbsp; <span>{{ selectedDataArray.studentId }}</span
-            ><br />
-            <span>{{ selectedDataArray.studentName }}</span
-            >&nbsp;同學
-          </label>
-        </div>
-      </div>
-      <div class="section2">
-        <div class="s2-content">
-          <div class="s2-text">
-            <p>{{ selectedDataArray.parent }}家長，您好</p>
-            <label>
-              貴子弟本次期中考受預警科目如下，請多留意貴子弟在校的學習狀況：<br />
-              <b>若有相關問題請與學系導師聯繫</b>。謝謝！ </label
-            ><br />
-            <div class="some-content"><p>慈濟大學教務處註冊組&nbsp;&nbsp;敬啟</p></div>
-
-            <div class="header">
-              <label>
-                <b
-                  ><span>{{ selectedDataArray.inputAcademicYear }}</span
-                  >學年度第<span>{{ selectedDataArray.inputSemester }}</span
-                  >學期期中考試預警通知</b
-                >
-              </label>
-            </div>
-            <br />
+  <div>
+    <div v-for="(data, index) in selectedDataArray" :key="index">
+      <div class="print-content">
+        <div class="section1">
+          <div class="s1-1">
             <div class="some-content">
-              <label>列印日期：{{ currentDate }}</label
-              ><br />
+              <p><b>【 限時專送 】</b></p>
+            </div>
+            <div class="s1-address">
+              <b
+                ><p>{{ data.warnings[0].address }}</p></b
+              >
+              <b> {{ data.warnings[0].parent }}&nbsp;先生/女士&nbsp;收 </b>
             </div>
           </div>
-          <label>
-            班級：<span>{{ departmentName[index] }}</span
-            ><span>{{ selectedDataArray.degree }}</span
-            >年<span>{{ selectedDataArray.studentClass }}</span
-            >班<br />
-            學號：<span>{{ selectedDataArray.studentId }}</span
-            ><br />
-            姓名：<span>{{ selectedDataArray.studentName }}</span>
-          </label>
+          <div class="s1-std">
+            <label class="s1-student">
+              <span>{{ data.warnings[0].department }}</span>
+              -<span>{{ data.warnings[0].degree }}</span> -<span>{{
+                data.warnings[0].studentClass
+              }}</span>
+              -<span>{{ data.warnings[0].studentId }}</span>
+              <br />
+              <span>{{ data.warnings[0].studentName }}</span>
+              &nbsp;同學
+            </label>
+          </div>
         </div>
-        <div>
-          <table class="course-table">
-            <thead class="table-header">
-              <th><b>課程名稱</b></th>
-              <th><b>學分數</b></th>
-            </thead>
-            <tbody>
-              <tr>
-                <td v-if="selectedDataArray && selectedDataArray.courseName">
-                  {{ selectedData.courseName }}
-                </td>
-                <td v-if="selectedDataArray && selectedDataArray.credit">
-                  {{ selectedDataArray.credit }}學分
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2" style="text-align: right; padding-right: 10px">
-                  <b style="display: block; text-align: right"
-                    >預警課程學分數合計：<span>{{ selectedDataArray.totalCredit }}</span
-                    >學分</b
+        <div class="section2">
+          <div class="s2-content">
+            <div class="s2-text">
+              <p>{{ data.warnings[0].parent }}家長，您好</p>
+              <label>
+                貴子弟本次期中考受預警科目如下，請多留意貴子弟在校的學習狀況：<br />
+                <b>若有相關問題請與學系導師聯繫</b>。謝謝！
+              </label>
+              <br />
+              <div class="some-content">
+                <p>慈濟大學教務處註冊組&nbsp;&nbsp;敬啟</p>
+              </div>
+              <div class="header">
+                <label>
+                  <b
+                    ><span>{{ data.inputAcademicYear }}</span> 學年度第
+                    <span>{{ data.inputSemester }}</span> 學期期中考試預警通知</b
                   >
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <p>
-            <b
-              >※&nbsp;本學期總修習學分數：<span>{{ selectedDataArray.totalCredit }}</span
-              >學分</b
-            >
-          </p>
+                </label>
+              </div>
+              <br />
+              <div class="some-content">
+                <label>列印日期：{{ currentDate }}</label
+                ><br />
+              </div>
+            </div>
+            <label>
+              班級：<span>{{ departmentName[index] }}&nbsp;</span>
+              <span>{{ data.warnings[0].degree }}</span>
+              年<span>{{ data.warnings[0].studentClass }}</span> 班<br />
+              學號：<span>{{ data.warnings[0].studentId }}</span
+              ><br />
+              姓名：<span>{{ data.warnings[0].studentName }}</span>
+            </label>
+          </div>
+          <div>
+            <table class="course-table">
+              <thead class="table-header">
+                <th style="width: 80%"><b>課程名稱</b></th>
+                <th style="width: 20%"><b>學分數</b></th>
+              </thead>
+              <tbody>
+                <tr v-for="(warning, wIndex) in data.warnings" :key="wIndex">
+                  <td style="padding-left: 7px">{{ warning.courseName }}</td>
+                  <td class="s2-credit">{{ warning.credit }}學分</td>
+                </tr>
+                <tr>
+                  <td colspan="2" style="text-align: right; padding-right: 7px">
+                    <b style="display: block; text-align: right">
+                      預警課程學分數合計：<span>{{ data.totalCredit }}</span
+                      >學分
+                    </b>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p>
+              <b
+                >※&nbsp;本學期總修習學分數：<span>{{ data.warnings[0].totalCredit }}</span
+                >學分</b
+              >
+            </p>
+          </div>
         </div>
       </div>
+      <div style="page-break-after: always"></div>
     </div>
-    <div style="page-break-after: always"></div>
   </div>
 </template>
 
@@ -108,13 +105,19 @@ import print from "vue3-print-nb";
 export default {
   name: "Print",
   directives: { print },
-  setup() {
+  props: {
+    selectedDataArray: Array,
+    departmentName: Array,
+    currentDate: String,
+  },
+  setup(props) {
     const apiDataStore = useApiDataStore();
     const currentDate = ref("");
     const selectedData = computed(() => apiDataStore.selectedData);
     const selectedDataArray = computed(() => {
       return apiDataStore.getSelectedData;
     });
+    const printedCounts = {};
 
     onMounted(() => {
       window.print();
@@ -200,7 +203,7 @@ export default {
         const warnings = item.warnings || [];
         for (const warning of warnings) {
           if (warning.department && deptName.hasOwnProperty(warning.department)) {
-            result.push(deptName[warning.department]);
+            result.push(deptName[warning.department].trim());
             break;
           }
         }
@@ -208,7 +211,20 @@ export default {
       return result;
     });
 
+    function shouldDisplayWarning(data, index) {
+      const studentId = data.studentId;
+      printedCounts[studentId] = printedCounts[studentId] || 0; // 初始化計數器
+
+      if (printedCounts[studentId] > 0) {
+        return false;
+      }
+
+      printedCounts[studentId]++;
+      return true;
+    }
+
     return {
+      shouldDisplayWarning,
       currentDate,
       departmentName,
       selectedDataArray,
@@ -317,5 +333,11 @@ td {
   flex-direction: column;
   justify-content: center;
   /* align-items: center;  */
+}
+.s1-student {
+  line-height: 1.3;
+}
+.s2-credit {
+  text-align: center;
 }
 </style>
